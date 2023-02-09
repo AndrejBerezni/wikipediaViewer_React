badc1b0b23debb0d6921b09770cf66df70614f8e
Andrej Berezni <berezniandrej@gmail.com>
2023-02-09 14:56:55 +0000

    styles and layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+function App() {
+  return (
+    <div id='app'>
+      <div className="button">
+        <p className="btnText">Read random article</p>
+        <div className="btnTwo">
+          <p className="btnText2"><i class="fas fa-search"></i></p>
+        </div>
+      </div>
+      <form id='search-form' href>
+        <input id='input' type='text' name='search' placeholder='Type your search here...'/>
+        <div className="button" id='form-button'>
+          <p className="btnText">Search</p>
+          <div className="btnTwo">
+            <p className="btnText2"><i class="fas fa-search"></i>
+</p>
+          </div>
+      </div>
+      </form>
+    </div>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +32,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
